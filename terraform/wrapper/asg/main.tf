2fbf255928c951efc161e32cd4928231cbad5877
Prakash <nishanps@682f67918242.ant.amazon.com>
2023-02-03 18:40:16 +0530

    application deployment

--- terraform/wrapper/asg/main.tf
+++ terraform/wrapper/asg/main.tf
@@ -54,6 +54,16 @@ module "test-app_autoscaling_group" {
   enable_monitoring         = lookup(local.asg_configurations["test-app"], "enable_monitoring")
   target_group_arns         = data.terraform_remote_state.alb.outputs.target_group_arns
   block_device_mappings     = lookup(local.asg_configurations["test-app"], "block_device_mappings")
+  instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+      checkpoint_delay       = 600
+      checkpoint_percentages = [35, 70, 100]
+      instance_warmup        = 300
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
+  }
   tag_specifications = [
     {
       resource_type = "instance"
